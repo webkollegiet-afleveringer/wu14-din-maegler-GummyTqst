bb8103e8f6760fd69ed6c6b3911e74aec145acbc
Tobias <neumsnn.tobias@gmail.com>
2026-04-13 12:26:36 +0200

    Updated Login and Register pages with new designs

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import HeroImage from "../assets/heroimage.png"
 import PageHeaderComponent from '../components/PageHeader';
 
@@ -14,8 +15,39 @@ export default function LoginPage() {
                 ]}    
             />
             <main className="max-w-7xl mx-auto p-4">
-                <h1 className="text-2xl font-bold mb-4">Login Page</h1>
-                <p>Please enter your credentials to log in.</p>
+                <section className="max-w-3xl mx-auto flex items-center flex-col gap-6 mb-8 border border-shape-1 rounded bg-white shadow-lg p-12">
+                    <h1 className="text-heading-2 text-head-2 mb-4">Log ind på din konto</h1>
+
+                    <form className="w-[60%]">
+                        <div className="flex flex-col gap-4 mb-6">
+                            <div>
+                                <label htmlFor="email" className="text-paragraph-1 text-para-1 mb-1">Email</label>
+                                <input type="email" placeholder="Email" className="w-full text-paragraph-2 text-para-1 border border-shape-1 p-2" />
+                            </div>
+
+                            <div>
+                                <label htmlFor="password" className="text-paragraph-1 text-para-1 mb-1">Password</label>
+                                <input type="password" placeholder="Password" className="w-full text-paragraph-2 text-para-1 border border-shape-1 p-2" />
+                            </div>
+                        </div>
+
+                        <button type="submit" className="bg-primary-1 text-white p-3.5 w-full">Log ind</button>
+                    </form>
+
+                    <div className="w-[60%] text-center">
+                        <p className="text-left">Log ind med</p>
+                        <div className="grid grid-cols-3 gap-3 mt-4">
+                            <button className="bg-red text-white px-6 py-3 rounded">Google</button>
+                            <button className="bg-[#3B5999] text-white px-6 py-3 rounded">Facebook</button>
+                            <button className="bg-primary-1 text-white px-6 py-3 rounded">Twitter</button>
+                        </div>
+
+                        <p className="mt-6 text-gray-600">
+                            Har du ikke en konto?
+                            <Link to={"/register"} className="text-blue-500 hover:underline"> Opret bruger.</Link>
+                        </p>
+                    </div>
+                </section>
             </main>
         
         </>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,48 @@
+import HeroImage from "../assets/heroimage.png"
+import PageHeaderComponent from '../components/PageHeader';
+
 export default function RegisterPage() {
     return (
-        <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4">Register Page</h1>
-        <p>Please fill in the form to create an account.</p>
-        </div>
+        <>
+            <PageHeaderComponent 
+                title="Account Login" 
+                imageSrc={HeroImage} 
+                breadcrumbs={[
+                    { label: "Home", path: "/" },
+                    { label: "Register", path: "/register", active: true }
+                ]}    
+            />
+            <main className="max-w-7xl mx-auto p-4">
+                <section className="max-w-3xl mx-auto flex items-center flex-col gap-6 mb-8 border border-shape-1 rounded bg-white shadow-lg p-12">
+                    <h1 className="text-heading-2 text-head-2 mb-4">Opret bruger hos Din Mægler</h1>
+
+                    <form className="w-[60%]">
+                        <div className="flex flex-col gap-4 mb-6">
+                            <div>
+                                <label htmlFor="name" className="text-paragraph-1 text-para-1 mb-1">Fulde navn</label>
+                                <input type="text" placeholder="Fulde navn" className="w-full text-paragraph-2 text-para-1 border border-shape-1 p-2" />
+                            </div>
+
+                            <div>
+                                <label htmlFor="email" className="text-paragraph-1 text-para-1 mb-1">Email adresse</label>
+                                <input type="email" placeholder="Email adresse" className="w-full text-paragraph-2 text-para-1 border border-shape-1 p-2" />
+                            </div>
+
+                            <div>
+                                <label htmlFor="password" className="text-paragraph-1 text-para-1 mb-1">Password</label>
+                                <input type="password" placeholder="Password" className="w-full text-paragraph-2 text-para-1 border border-shape-1 p-2" />
+                            </div>
+
+                            <div>
+                                <label htmlFor="confirmpassword" className="text-paragraph-1 text-para-1 mb-1">Bekræft password</label>
+                                <input type="password" placeholder="Bekræft password" className="w-full text-paragraph-2 text-para-1 border border-shape-1 p-2" />
+                            </div>
+                        </div>
+
+                        <button type="submit" className="bg-primary-1 text-white p-3.5 w-full">Opret bruger</button>
+                    </form>
+                </section>
+            </main>
+        </>
     )
 }
